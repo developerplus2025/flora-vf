d733f7faab918eb7e4e16e2e448e6035c290e814
devplus2024 <anpham08112009@gmail.com>
2025-03-07 17:07:07 +0700

    fg

--- src/app/home/components/audio-card.tsx
+++ src/app/home/components/audio-card.tsx
@@ -164,8 +164,11 @@ const AudioCard = () => {
             onMouseDown={handlemousedown}
             onMouseUp={handlemouseup}
             className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20"
-          ></Slider.Track>
-          <Slider.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+          >
+            {" "}
+            <Slider.Range className="absolute h-full bg-primary" />
+          </Slider.Track>
+          <Slider.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
         </Slider.Root>
         <p className="text-sm tabular-nums">{currentTimeMusic}</p>
       </div>
