cc261a0afaffe3eeebce969752acc9091d3e662e
devplus2024 <anpham08112009@gmail.com>
2025-03-07 16:35:18 +0700

    cv

--- src/app/home/components/audio-card.tsx
+++ src/app/home/components/audio-card.tsx
@@ -121,15 +121,13 @@ const AudioCard = () => {
           value={[(currentTime / totalSeconds) * 100]}
           max={100}
           step={1}
-          className="relative flex w-[20rem] touch-none select-none items-center"
+          className="relative flex w-full touch-none select-none items-center"
         >
           <Slider.Track
             onMouseDown={handlemousedown}
             onMouseUp={handlemouseup}
             className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20"
-          >
-            <Slider.Range className="absolute h-full bg-primary" />
-          </Slider.Track>
+          ></Slider.Track>
           <Slider.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
         </Slider.Root>
         <p className="text-sm tabular-nums">{currentTimeMusic}</p>
