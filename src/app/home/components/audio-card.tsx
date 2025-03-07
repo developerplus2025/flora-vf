f793b9c8107718cce1b16a41d1a05e9c87bc866f
devplus2024 <anpham08112009@gmail.com>
2025-03-07 16:44:30 +0700

    bn

--- src/app/home/components/audio-card.tsx
+++ src/app/home/components/audio-card.tsx
@@ -126,7 +126,7 @@ const AudioCard = () => {
           <Slider.Track
             onMouseDown={handlemousedown}
             onMouseUp={handlemouseup}
-            className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20"
+            className="relative h-1.5 w-[20rem] grow overflow-hidden rounded-full bg-primary/20"
           ></Slider.Track>
           <Slider.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
         </Slider.Root>
