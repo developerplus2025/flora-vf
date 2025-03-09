446a53a974413258a15d6c351dbaa7b117272678
devplus2024 <anpham08112009@gmail.com>
2025-03-09 09:59:29 +0700

    cv

--- src/app/home/components/audio-card.tsx
+++ src/app/home/components/audio-card.tsx
@@ -79,7 +79,7 @@ const AudioCard = () => {
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
   return (
-    <div className="mt-[2rem] flex w-[700px] items-center justify-between gap-[1rem] rounded-lg border bg-[#131313] px-[1rem] py-2">
+    <div className="mt-[2rem] flex w-[800px] items-center justify-between gap-[1rem] rounded-lg border bg-[#131313] px-[1rem] py-2">
       <audio
         id="audio"
         ref={audioRef}
@@ -172,8 +172,9 @@ const AudioCard = () => {
         </Slider.Root>
         <p className="text-xs tabular-nums">{currentTimeMusic}</p>
       </div>
-      <div>
+      <div className="flex w-[170px] items-center gap-[1rem]">
         <svg
+          className="flex-shrink-0"
           data-testid="geist-icon"
           height={16}
           strokeLinejoin="round"
@@ -188,6 +189,24 @@ const AudioCard = () => {
             fill="currentColor"
           />
         </svg>
+        <Slider.Root
+          onValueChange={(newSoundValue) => {
+            setSoundValue(newSoundValue);
+          }}
+          value={[Number(soundValue)]}
+          max={100}
+          step={1}
+          className="relative flex w-[6rem] touch-none select-none items-center"
+        >
+          <Slider.Track
+            onMouseDown={handlemousedown}
+            onMouseUp={handlemouseup}
+            className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20"
+          >
+            <Slider.Range className="absolute h-full bg-primary" />
+          </Slider.Track>
+          <Slider.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+        </Slider.Root>
       </div>
       <div>
         <svg
