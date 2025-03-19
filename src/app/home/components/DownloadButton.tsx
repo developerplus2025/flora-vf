280eda9cf983abe68ab45d60c96101c5f1e56aad
devplus2024 <anpham08112009@gmail.com>
2025-03-19 17:30:15 +0700

    vb

--- src/app/home/components/DownloadButton.tsx
+++ src/app/home/components/DownloadButton.tsx
@@ -15,7 +15,11 @@ const DownloadButton: React.FC = () => {
     setDownloadReady(true);
   };
   return (
-    <Button variant={"outline"} onClick={handleClick}>
+    <Button
+      className="bg-[#131313] ring-1 ring-[##c6c6c680] ring-offset-1 ring-offset-black"
+      variant={"outline"}
+      onClick={handleClick}
+    >
       <ArrowDownToLine className="mr-2 h-4 w-4" />
       Download for Windows
     </Button>
