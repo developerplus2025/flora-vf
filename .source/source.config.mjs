959b4dc59e9584a39970a454f1b5aca4cf0ee2c3
devplus2024 <anpham08112009@gmail.com>
2025-03-22 14:30:55 +0700

    vb

--- .source/source.config.mjs
+++ .source/source.config.mjs
@@ -1,5 +1,11 @@
 // source.config.ts
-import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import {
+  defineDocs,
+  defineConfig,
+  defineCollections,
+  frontmatterSchema
+} from "fumadocs-mdx/config";
+import { z } from "zod";
 var docs = defineDocs({
   dir: "content/docs"
 });
@@ -8,7 +14,17 @@ var source_config_default = defineConfig({
     // MDX options
   }
 });
+var blogPosts = defineCollections({
+  type: "doc",
+  dir: "content/blog",
+  // add required frontmatter properties
+  schema: frontmatterSchema.extend({
+    author: z.string(),
+    date: z.string().date().or(z.date())
+  })
+});
 export {
+  blogPosts,
   source_config_default as default,
   docs
 };
--- source.config.ts
+++ source.config.ts
@@ -1,11 +1,26 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import {
+  defineDocs,
+  defineConfig,
+  defineCollections,
+  frontmatterSchema,
+} from "fumadocs-mdx/config";
+import { z } from "zod";
 
 export const docs = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
 });
 
 export default defineConfig({
   mdxOptions: {
     // MDX options
   },
+});
+export const blogPosts = defineCollections({
+  type: "doc",
+  dir: "content/blog",
+  // add required frontmatter properties
+  schema: frontmatterSchema.extend({
+    author: z.string(),
+    date: z.string().date().or(z.date()),
+  }),
 });
