6e578f13ad6d493d2fa0e68f2fa59a137093a78d
devplus2024 <anpham08112009@gmail.com>
2025-03-22 18:57:20 +0700

    cv

--- src/components/comp-410.tsx
+++ src/components/comp-410.tsx
@@ -4,28 +4,28 @@ export default function CompAvatarListBlog() {
   return (
     <div className="flex -space-x-3">
       <img
-        className="rounded-full ring-2 ring-background"
+        className="h-[20px] w-[20px] rounded-full ring-2 ring-background"
         src="/avatar-80-03.jpg"
         width={20}
         height={20}
         alt="Avatar 01"
       />
       <img
-        className="rounded-full ring-2 ring-background"
+        className="h-[20px] w-[20px] rounded-full ring-2 ring-background"
         src="/avatar-80-04.jpg"
         width={20}
         height={20}
         alt="Avatar 02"
       />
       <img
-        className="rounded-full ring-2 ring-background"
+        className="h-[20px] w-[20px] rounded-full ring-2 ring-background"
         src="/avatar-80-05.jpg"
         width={20}
         height={20}
         alt="Avatar 03"
       />
       <img
-        className="rounded-full ring-2 ring-background"
+        className="h-[20px] w-[20px] rounded-full ring-2 ring-background"
         src="/avatar-80-06.jpg"
         width={20}
         height={20}
