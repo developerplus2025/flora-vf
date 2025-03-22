16b633297b9def5d576d7e19aafa33fb9dc5b866
devplus2024 <anpham08112009@gmail.com>
2025-03-22 20:04:00 +0700

    fg

--- src/components/comp-410.tsx
+++ src/components/comp-410.tsx
@@ -4,28 +4,28 @@ export default function CompAvatarListBlog() {
   return (
     <div className="flex -space-x-3">
       <img
-        className="h-[20px] w-[20px] rounded-full ring-2 ring-background"
+        className="h-[25px] w-[25px] rounded-full ring-2 ring-background"
         src="/avatar-80-03.jpg"
         width={20}
         height={20}
         alt="Avatar 01"
       />
       <img
-        className="h-[20px] w-[20px] rounded-full ring-2 ring-background"
+        className="h-[25px] w-[25px] rounded-full ring-2 ring-background"
         src="/avatar-80-04.jpg"
         width={20}
         height={20}
         alt="Avatar 02"
       />
       <img
-        className="h-[20px] w-[20px] rounded-full ring-2 ring-background"
+        className="h-[25px] w-[25px] rounded-full ring-2 ring-background"
         src="/avatar-80-05.jpg"
         width={20}
         height={20}
         alt="Avatar 03"
       />
       <img
-        className="h-[20px] w-[20px] rounded-full ring-2 ring-background"
+        className="h-[25px] w-[25px] rounded-full ring-2 ring-background"
         src="/avatar-80-06.jpg"
         width={20}
         height={20}
@@ -33,7 +33,7 @@ export default function CompAvatarListBlog() {
       />
       <Button
         variant="secondary"
-        className="flex size-10 items-center justify-center rounded-full bg-secondary text-xs text-muted-foreground ring-2 ring-background hover:bg-secondary hover:text-foreground"
+        className="flex h-[25px] w-[25px] items-center justify-center rounded-full bg-secondary text-xs text-muted-foreground ring-2 ring-background hover:bg-secondary hover:text-foreground"
         size="icon"
       >
         +3
