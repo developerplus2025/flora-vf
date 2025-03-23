db27470be9e405360928f3a1591834205be01e8d
devplus2024 <anpham08112009@gmail.com>
2025-03-23 21:18:22 +0700

    vb

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,4 +1,10 @@
+import { AnimatedTabs } from "@/components/AnimationTab";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import type { Metadata } from "next";
+import Link from "next/link";
+
 export const metadata: Metadata = {
   title: "Blog - Flora",
   description: "Blog - Flora",
@@ -8,5 +14,30 @@ export default function BlogLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <section>{children}</section>;
+  return (
+    <section>
+      <div className="flex w-full items-center justify-between gap-[4rem] px-[10rem]">
+        <h2 className="flex-shrink-0 text-nowrap text-2xl font-bold tracking-tight">
+          Recent Articles
+        </h2>
+        <AnimatedTabs />
+        <div className="relative w-full">
+          <Input
+            type="search"
+            placeholder="Search for favorite songs"
+            className="w-full rounded-full pl-[3rem] placeholder:text-[#7c7c7c]"
+          />
+          <MagnifyingGlassIcon
+            width="21"
+            height="21"
+            className="search_input-blog absolute left-[16px] top-1/2 -translate-y-1/2"
+          />
+        </div>
+        <Button variant="outline" asChild>
+          <Link href="#">View All</Link>
+        </Button>
+      </div>
+      {children}
+    </section>
+  );
 }
