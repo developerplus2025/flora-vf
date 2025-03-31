5af2eb5780514746ec1aa3f6d5ec91bf82ea6f89
devplus2024 <anpham08112009@gmail.com>
2025-03-31 17:26:23 +0700

    vb

--- src/app/blog/AnimationTab.tsx
+++ src/app/blog/AnimationTab.tsx
@@ -3,11 +3,12 @@
 import { useRouter, usePathname } from "next/navigation";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
 const TABS = [
-  { id: 1, label: "All Posts", link: "/blog/" },
-  { id: 2, label: "Interactions", link: "/blog/category/interactions/" },
-  { id: 3, label: "Resources", link: "/blog/category/resources/" },
-  { id: 4, label: "Docs", link: "/blog/category/docs/" },
+  { id: 1, label: "All Posts", link: "/" },
+  { id: 2, label: "Interactions", link: "/category/interactions/" },
+  { id: 3, label: "Resources", link: "/category/resources/" },
+  { id: 4, label: "Docs", link: "/category/docs/" },
 ];
 
 const normalizePath = (path: string) => path.replace(/\/$/, "");
@@ -18,28 +19,30 @@ export function AnimatedTabs() {
   return (
     <div className="flex space-x-1">
       {TABS.map((tab) => (
-        <button
-          key={tab.id}
-          onClick={() => {
-            setActiveTab(tab.id), router.push(tab.link);
-          }}
-          className={`${
-            activeTab === tab.id ? "" : "hover:text-white/60"
-          } relative flex h-8 items-center justify-center text-nowrap rounded-full p-3 text-sm font-medium text-white outline-sky-400 transition focus-visible:outline-2`}
-          style={{
-            WebkitTapHighlightColor: "transparent",
-          }}
-        >
-          {activeTab === tab.id && (
-            <motion.span
-              layoutId="bubble"
-              className="absolute inset-0 z-10 bg-white mix-blend-difference"
-              style={{ borderRadius: 9999 }}
-              transition={{ duration: 0.25 }}
-            />
-          )}
-          {tab.label}
-        </button>
+        <Link href={`${tab.link}`} key={tab.id}>
+          <button
+            key={tab.id}
+            onClick={() => {
+              setActiveTab(tab.id);
+            }}
+            className={`${
+              activeTab === tab.id ? "" : "hover:text-white/60"
+            } relative flex h-8 items-center justify-center text-nowrap rounded-full p-3 text-sm font-medium text-white outline-sky-400 transition focus-visible:outline-2`}
+            style={{
+              WebkitTapHighlightColor: "transparent",
+            }}
+          >
+            {activeTab === tab.id && (
+              <motion.span
+                layoutId="bubble"
+                className="absolute inset-0 z-10 bg-white mix-blend-difference"
+                style={{ borderRadius: 9999 }}
+                transition={{ duration: 0.25 }}
+              />
+            )}
+            {tab.label}
+          </button>
+        </Link>
       ))}
     </div>
   );
