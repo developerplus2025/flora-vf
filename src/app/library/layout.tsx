0dbd46c3ff3a5fd26280aa307578e0454258ea68
developerplus2025 <anpham08112009@gmail.com>
2025-04-19 21:08:43 +0700

    vb

--- src/app/library/layout.tsx
+++ src/app/library/layout.tsx
@@ -11,19 +11,19 @@ export default function LibraryLayout({
 }) {
   return (
     <section className="flex flex-col items-center gap-[3rem] px-[3rem] py-[5rem]">
-      <div className="flex flex-col gap-[3rem]">
+      <div className="flex flex-col items-center gap-[3rem]">
         <div className="flex flex-col items-center gap-[3rem]">
-          <p className="text-center text-4xl font-bold">
+          <p className="w-[838px] text-center text-[57px] font-bold leading-[3.5rem]">
             {" "}
             Explore a Powerful Sound Library - Crafted for Creators
           </p>
-          <span className="w-[450px] text-center text-xl font-semibold">
+          <span className="w-[530px] text-center text-xl font-semibold">
             Discover a rich collection of high-quality sounds, loops, and
             effects tailored for music producers. Instantly preview, customize,
             and add them to your projects — all within one seamless platform.
           </span>
         </div>
-        <div className="flex gap-4">
+        <div className="flex gap-[2rem]">
           <Button>Start Listening</Button>
           <Button variant={"outline"}>Browse Sounds</Button>
         </div>
